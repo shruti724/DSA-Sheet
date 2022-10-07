c9e7ff9a31ccd193ee61cfd3ff0ffd6d17ed44d8
S.R JHA <shrutiranjanjha274@gmail.com>
2022-10-07 22:53:22 +0530

    union program

---
+++ union_and_intersection.py
@@ -0,0 +1,41 @@
+
+# def union(lis1, lis2):
+#     s = set()
+#     for i in range(n):
+#         s.add(lis1[i])
+#
+#     for j in range(m):
+#         s.add(lis2[j])
+#     print(s, end="")
+#
+# t = int(input())
+# for i in range(t):
+#     n = int(input())
+#     arr1 = [int(x) for x in input().strip().split()]
+#     m = int(input())
+#     arr2 = [int(x) for x in input().strip().split()]
+#     union(arr1, arr2)
+
+# op:
+# 1
+# 5
+# 1 2 3 4 5
+# 6
+# 1 2 3 4 5 6
+# {1, 2, 3, 4, 5, 6}
+
+
+
+
+
+ 
+
+
+
+# s = set()
+# q = set()
+# s.add(1)
+# q.add(3)
+#
+# print(s,q)
+
