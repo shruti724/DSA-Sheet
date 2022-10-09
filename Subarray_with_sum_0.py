cfa4f91490c20e9b95bbf2c772ff097c673940c4
S.R JHA <shrutiranjanjha274@gmail.com>
2022-10-09 12:09:48 +0530

    Updated

--- Subarray_with_sum_0.py
+++ Subarray_with_sum_0.py
@@ -1,25 +1,28 @@
 def zeroSum(lis):
-    initialsum = 0
     curr = 0
     is_sum_zero = False
     for j in range(0, len(lis)):
-        initialsum = curr
-        for k in range(0, len(lis)):
-            if curr == 0:
-                is_sum_zero = True
-            else:
-                is_sum_zero = False
-    print(is_sum_zero)
+        curr = curr + lis[j]
+        if curr == 0:
+            is_sum_zero = True
+        else:
+            is_sum_zero = False
+    return is_sum_zero
 
 
 t = int(input())
 for i in range(t):
     n = int(input())
     arr = [int(x) for x in input().strip().split()]
-    zeroSum(arr)
+    print(zeroSum(arr))
 
 # op:
 # 1
-# 5
-# -1 1 2 3 4
+# 4
+# -1 2 3 -4
 # True
+
+# 1
+# 4
+# 1 2 3 4
+# False
